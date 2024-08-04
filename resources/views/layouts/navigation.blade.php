62600a505cd6374f061a94458b1da0f54708b44d
Aram Petrosyan <petrossian1995@mail.ru>
2024-08-04 23:30:22 +0400

    changes commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="/home">
+                    <a href="/">
                         <img class="block h-9 w-auto fill-current text-gray-800" src="{{asset('logo-blue.png')}}" alt="logo"/>
                     </a>
                 </div>
