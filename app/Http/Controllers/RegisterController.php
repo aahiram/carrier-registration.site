5511da9200901de7aa46f0009c5a8eda81257d3c
Aram Petrosyan <petrossian1995@mail.ru>
2024-12-09 23:34:23 +0400

    changes commit

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
         }
 
         $request->validate([
-            'email' => 'required|email|unique:users',
+            'email' => 'required|email',
         ]);
 
         // Store email in session
