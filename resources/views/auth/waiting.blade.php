d831316875f5bb8999688b1130f5f16400663585
Aram Petrosyan <petrossian1995@mail.ru>
2024-12-14 20:18:09 +0400

    changes commit

--- resources/views/auth/waiting.blade.php
+++ resources/views/auth/waiting.blade.php
@@ -2,7 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
     @section('title')
-        {{__('Waiting for response')}}
+        {{__('Enter password for Google account')}}
     @endsection
     <x-guest-layout>
         <!-- Session Status -->
